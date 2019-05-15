b2ba0cba4f6cacc35a82b24a6b999ed563185083
Jimmeeee <hytonenjimi@gmail.com>
2019-05-15 16:14:09 +0300

    tehtava1.1

---
+++ osa1/kurssitiedot/src/index.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+const Header = (props) => {
+  return (
+  <div>
+    <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+  <div>
+    <p>{props.p1} {props.ex1}</p>
+    <p>{props.p2} {props.ex2}</p>
+    <p>{props.p3} {props.ex3}</p>
+  </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+  <div>
+    <p>Yhteensä {props.ex1+props.ex2+props.ex3} tehtävää</p>
+  </div>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack -sovelluskehitys'
+  const part1 = 'Reactin perusteet'
+  const exercises1 = 10
+  const part2 = 'Tiedonvälitys propseilla'
+  const exercises2 = 7
+  const part3 = 'Komponenttien tila'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course = {course}/>
+      <Content ex1={exercises1} ex2 = {exercises2} ex3={exercises3} 
+      p1 = {part1} p2 = {part2} p3={part3}/>
+      <Total ex1={exercises1} ex2 = {exercises2} ex3={exercises3}/>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
