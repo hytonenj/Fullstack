3cc01a6f22910b63a03583976df3e13c900aef86
Jimmeeee <hytonenjimi@gmail.com>
2019-05-17 12:16:34 +0300

    tehtävä 1.13ja1.14 valmis

--- osa1/anekdootit/src/index.js
+++ osa1/anekdootit/src/index.js
@@ -3,32 +3,65 @@ import ReactDOM from 'react-dom'
 
 const App = (props) => {
   const [selected, setSelected] = useState(0)
+  const [vote, setVote] = useState(Array.apply(null, new Array(10)).map(Number.prototype.valueOf, 0))
+
+  const handleVote = () => {
+    const copy = [...vote]
+    copy[selected] += 1
+    return (
+      setVote(copy)
+    )
+  }
+
   return (
     <div>
       {props.anecdotes[selected]}
       <br></br>
-      <Button handleClick={()=>setSelected(getRandomInt(props.anecdotes.length))}/>
+      <p>has {vote[selected]} votes</p>
+      <Button handleClick={handleVote} text='vote' />
+      <Button handleClick={() => setSelected(getRandomInt(props.anecdotes.length))} text='next anecdote' />
+      <BestAnecdote vlist={vote} alist={props.anecdotes}/>
     </div>
   )
-  
+
 }
 
-const Button = (props)=>{
-  console.log
+const BestAnecdote = (props) => {
+  const votelist = props.vlist
+  const anecdotelist = props.alist
+  var mostVotes = 0
+
+  var max = votelist[0];
+  var maxIndex = 0;
+
+  for (var i = 1; i < votelist.length; i++) {
+    if (votelist[i] > max) {
+      maxIndex = i;
+      max = votelist[i];
+    }
+  }
   return(
-    <button onClick = {props.handleClick}>
-      Satunnainen
+    <div>
+      <h3>Anecdote with most votes</h3>
+      {anecdotelist[maxIndex]}
+      <br></br>
+      has {max} votes
+    </div>
+  )
+
+}
+
+const Button = (props) => {
+
+  return (
+    <button onClick={props.handleClick}>
+      {props.text}
     </button>
   )
 }
 
 const getRandomInt = (max) => Math.floor(Math.random() * Math.floor(max))
 
-/*
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
-*/
 const anecdotes = [
   'If it hurts, do it more often',
   'Adding manpower to a late software project makes it later!',
