b38c3bc2180f9435848b510a4284d982482f35cd
Jimmeeee <hytonenjimi@gmail.com>
2019-05-20 14:11:37 +0300

    tehtava 2.8

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,13 +2,15 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Jimi',
+    number: '045-000000'}
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
   // list persons function
   const rows = () => persons.map(person =>
-    <Person key={person.name} name = {person.name}/>
+    <Person key={person.name} name = {person.name} number={person.number}/>
   )
 
   //adding person
@@ -17,17 +19,25 @@ const App = () => {
     //console.log(persons.name);
     
     const personObject = {
-      name : newName
+      name : newName,
+      number : newNumber
     }
     persons.some(e=>e.name===newName)?window.alert(`${newName} on jo luettelossa`):
     setPersons(persons.concat(personObject))
     setNewName('')
+    setNewNumber('')
   }
 
   //name change
   const handleNameChange = (event) =>{
     //console.log(event.target.value)
     setNewName(event.target.value)
+  }
+
+  //number change
+  const handleNumberChange = (event) =>{
+    //console.log(event.target.value)
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -37,6 +47,9 @@ const App = () => {
         <div>
           nimi: <input value = {newName}
           onChange = {handleNameChange}/>
+          <br></br>
+          numero: <input value = {newNumber}
+          onChange = {handleNumberChange}/>
         </div>
         <div>
           <button type="submit">lisää</button>
@@ -51,7 +64,7 @@ const App = () => {
 
 const Person = (props) =>{
   return(
-    <li>{props.name}</li>
+    <li>{props.name} {props.number}</li>
     
   )
 }
