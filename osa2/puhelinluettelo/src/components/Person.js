e454e433474520616b2ee66ee1697f948d40d82d
Jimmeeee <hytonenjimi@gmail.com>
2019-05-24 13:32:40 +0300

    osa3-t3.9

--- osa2/puhelinluettelo/src/components/Person.js
+++ osa2/puhelinluettelo/src/components/Person.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 
 const Person = (props) => {
-    
-    return (
-      <li>{props.name} {props.number} <button onClick={()=>props.removePerson(props.id)}>poista</button></li>
-  
-    )
-  }
 
-  export default Person
+    return (
+        <li>{props.name} {props.number} <button onClick={() => props.removePerson(props.id)}>poista</button></li>
+
+    )
+}
+
+export default Person
